edd69d70fcd36d078dbdcba039ddaf486f00cb1f
Lucas Oliveira <lfo@icomp.ufam.edu.br>
2020-02-21 07:25:15 -0800

    Restructuring

--- frontend/app.mjs
+++ frontend/app.mjs
@@ -1,11 +1,3 @@
-import {carouselMostPopular} from './src/carousel-mostpopular.component.mjs'
-import {carouselPriceReduction} from './src/carousel-pricereduction.component.mjs'
-
 export function app() {
-    startCarousels()
-}
-
-function startCarousels() {
-    carouselMostPopular(document)
-    carouselPriceReduction(document)
+    return
 }
